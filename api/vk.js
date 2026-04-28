6823e01dbb94a4c58168f052d00c4af9d0c9a804
bqtarget-dev <bqtarget@gmail.com>
2026-04-28 18:57:15 +0300

    Create vk.js

---
+++ api/vk.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+
+  const { path, ...params } = req.query;
+  const query = new URLSearchParams(params).toString();
+  const url = `https://api.vk.com/method/${path}?${query}`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch (e) {
+    res.status(500).json({ error: 'Proxy error' });
+  }
+}
