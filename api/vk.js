e2035607de0d69da3848c3667d2e70fbe6617c80
bqtarget-dev <bqtarget@gmail.com>
2026-04-28 19:15:48 +0300

    Update vk.js

--- api/vk.js
+++ api/vk.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
       }
     });
     const data = await response.json();
-    res.status(200).json(data);
+    res.status(200).json({ raw: data, status: response.status });
   } catch (e) {
     res.status(500).json({ error: 'Proxy error', message: e.message });
   }
